ea8fa83c5bba9d17c7a8288aa93b3057a176ffd3
Sheikhmsip <sheikhmsip@gmail.com>
2023-06-09 00:42:21 +0600

    connect mongodb and create user

--- index.js
+++ index.js
@@ -9,12 +9,63 @@ app.use(cors());
 app.use(express.json());
 
 
+// Pass: DObLeftNP0by7thh
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<username>:<password>@cluster0.vvvwsgj.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://summerCamp:DObLeftNP0by7thh@cluster0.vvvwsgj.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const usersCollection = client.db("summerCamp").collection("users");
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+
+    });
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
-    res.send('Summer camp is sitting')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Summer-camp is sitting on port ${port}`)
-  })
-  
+  res.send('Summer camp is sitting')
+})
+
+app.listen(port, () => {
+  console.log(`Summer-camp is sitting on port ${port}`)
+})
