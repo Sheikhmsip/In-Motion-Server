5c5b678c26c1dbcceff20a1491ef2acc78983cf7
Sheikhmsip <sheikhmsip@gmail.com>
2023-06-11 20:30:33 +0600

    create admin related api

--- index.js
+++ index.js
@@ -128,6 +128,27 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+    
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+    
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+    // User Delete Api 
+    app.delete('/user-delete/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+    
 
 
     // Classes API 
